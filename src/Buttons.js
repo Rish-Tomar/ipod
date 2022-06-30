f2ca81dd45a3c3cde0b5aa278ce4caff2894d2c2
Rishabh-dot-tomar <70138890+Rishabh-dot-tomar@users.noreply.github.com>
2022-07-01 05:01:53 +0530

    css of ipod completed

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,18 +1,23 @@
 import React from "react";
+import FastForwardIcon from '@mui/icons-material/FastForward';
+import FastRewindIcon from '@mui/icons-material/FastRewind';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 const Buttons =()=>{
     return(
         <div className="buttons">
             <div className="buttons-container">
                 <div className="inner-div">
-                    {/* <h1>inner div</h1> */}
-                    
-                    
-
+                   
+                    <span class="menu-btn">MENU</span>
+                    <FastForwardIcon className="fwd"/>
+                    <FastRewindIcon  className="rewind" />         
+                    <PlayArrowIcon className="play-pause"/>
+                                       
                     <div className="inner-circle">
                     
-                </div>
-                </div>
+                    </div>
+              </div>
                
             </div>
         </div>

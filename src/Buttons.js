5819b8e9baa89695aa1b16ac6396e7519358abd5
Rishabh-dot-tomar <70138890+Rishabh-dot-tomar@users.noreply.github.com>
2022-07-01 04:12:35 +0530

    css of ipod layout made

---
+++ src/Buttons.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Buttons =()=>{
+    return(
+        <div className="buttons">
+            <div className="buttons-container">
+                <div className="inner-div">
+                    {/* <h1>inner div</h1> */}
+                    
+                    
+
+                    <div className="inner-circle">
+                    
+                </div>
+                </div>
+               
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
+export default Buttons
